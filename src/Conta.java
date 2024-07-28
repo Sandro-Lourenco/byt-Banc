6786c6c595650d88b23fb4a523a54231d6ad7073
sandro <sandrocalebe8@gmail.com>
2024-07-28 11:33:34 -0300

    adicionando os metodos gethers e sethers

--- src/Conta.java
+++ src/Conta.java
@@ -19,7 +19,6 @@ public class Conta {
             return true;
         }
         return false;
-
     }
 
     public boolean transferir(double valor, Conta destino){
@@ -29,6 +28,47 @@ public class Conta {
             return true;
         }
         return false;
-        
     }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getNumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    public void setNumeroDaConta(int numeroDaConta) {
+        this.numeroDaConta = numeroDaConta;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNume() {
+        return nume;
+    }
+
+    public void setNume(int nume) {
+        this.nume = nume;
+    }
+
+    
 }
--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,12 @@ public class Main {
         Conta c1 = new Conta();
         c1.saldo = 300;
         //c1.sacar(20);
-        c1.depositar(10);
-        System.out.println(c2.saldo);
-        System.out.println(c1.saldo);
+        //c1.depositar(10);
+        c1.transferir(100, c2);
+
+
+        System.out.println(c2.saldo);//400
+        System.out.println(c1.saldo);//200
 
      
 
